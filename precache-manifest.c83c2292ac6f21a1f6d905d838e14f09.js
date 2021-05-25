d8457acc0d14826fd29edee9d40e2c9171c09775
james.jeon <no@email.com>
2021-05-25 17:31:30 -0500

    Updates

---
+++ precache-manifest.c83c2292ac6f21a1f6d905d838e14f09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eba8b3bf62aac8e7099cd6890b0659a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afb0adf089181df71a0b",
+    "url": "/static/js/2.41139a83.chunk.js"
+  },
+  {
+    "revision": "d0cb0a244b3563727e54b0f5d5982212",
+    "url": "/static/js/2.41139a83.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d53563b0ac23a80c8ea9",
+    "url": "/static/js/main.8fef6327.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
