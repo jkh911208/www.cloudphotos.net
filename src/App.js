aa2a9bd3ce3c428e1986931cf92edfbcfe78c06a
james.jeon <no@email.com>
2021-05-26 22:11:22 -0500

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Support from "./components/pages/Support";
 import Privacy from "./components/pages/Privacy";
@@ -8,7 +8,7 @@ import Navbar from "./components/layout/Navbar";
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Fragment>
         <Navbar />
         <Switch>
@@ -25,7 +25,7 @@ function App() {
           />
         </Switch>
       </Fragment>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
