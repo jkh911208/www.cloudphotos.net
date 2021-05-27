99a46f2cb78f4cebe95cab935ff0c6331b44f9b8
james.jeon <no@email.com>
2021-05-26 21:59:04 -0500

    add 404

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Support from "./components/pages/Support";
 import Privacy from "./components/pages/Privacy";
@@ -8,7 +8,7 @@ import Navbar from "./components/layout/Navbar";
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Fragment>
         <Navbar />
         <Switch>
@@ -25,7 +25,7 @@ function App() {
           />
         </Switch>
       </Fragment>
-    </Router>
+    </BrowserRouter>
   );
 }
 
